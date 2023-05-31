8f984a8b21a6b226975919350ffeda189e923080
albertomoiseis <contatoalbertomoiseis@gmail.com>
2023-05-31 17:32:07 -0300

    add: Adicionando os elementos na tela de forma dinamica

---
+++ src/main.js
@@ -0,0 +1,36 @@
+import data from './data/animes/one_piece_quote.json' assert {type: "json"}
+/*
+const animes = data
+
+function showAnimes(itens){
+  document.getElementById("animes_one_piece").innerHTML = todos(itens)
+}
+console.table(showAnimes(data.itens))
+
+function todos(data){
+   data.map((item)=>
+    {
+      return `
+      <div class="cards">
+        <p>${item.anime}</p>
+        <p>${item.character}</p>
+        <addr>${item.quote}</addr>
+      </div>
+    `
+  })
+}
+*/
+const animes =document.getElementById("animes_one_piece")
+
+data.forEach(function(item){
+  animes.innerHTML +=   
+  `
+    <div class="cards">
+     <div>
+        <p>${item.anime}</p>
+        <p>${item.character}</p>
+        <addr>${item.quote}</addr>
+      </div>
+    </div>
+  `
+})
